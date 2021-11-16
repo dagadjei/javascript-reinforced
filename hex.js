9e50ae8d709d0e1770adf59b58351c48f606f324
unknown <adjeidag@gmail.com>
2021-11-16 23:20:05 +0000

    new file

--- hex.js
+++ hex.js
@@ -4,6 +4,15 @@ const btn = document.querySelector("#btn");
 const color = document.querySelector(".color");
 
 
+btn.addEventListener("click", function(){
+    let newColor = "#";
+    for(let i=0; i<6; i++){
+        newColor += hex[random()]
+    }
+    document.body.style.backgroundColor = newColor
+    color.textContent = newColor
+});
+
 function random(){
     let num = Math.floor(Math.random()*hex.length)
     return num
